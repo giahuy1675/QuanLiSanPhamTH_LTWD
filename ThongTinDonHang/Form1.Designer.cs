350eb4ab9ed783d21fcf753e7a3fba5c34826764
giahu <giahu@ADMIN>
2024-10-15 23:14:46 +0700

    Add project files.

---
+++ ThongTinDonHang/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ThongTinDonHang
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cb_XemTatCaDonHang = new System.Windows.Forms.CheckBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txb_TongDonHang = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cb_XemTatCaDonHang
+            // 
+            this.cb_XemTatCaDonHang.AutoSize = true;
+            this.cb_XemTatCaDonHang.Location = new System.Drawing.Point(608, 16);
+            this.cb_XemTatCaDonHang.Name = "cb_XemTatCaDonHang";
+            this.cb_XemTatCaDonHang.Size = new System.Drawing.Size(126, 20);
+            this.cb_XemTatCaDonHang.TabIndex = 0;
+            this.cb_XemTatCaDonHang.Text = "Tất cả đơn hàng";
+            this.cb_XemTatCaDonHang.UseVisualStyleBackColor = true;
+            this.cb_XemTatCaDonHang.CheckedChanged += new System.EventHandler(this.cb_XemTatCaDonHang_CheckedChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(200, 48);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 1;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(416, 48);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker2.TabIndex = 2;
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 80);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(768, 310);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // txb_TongDonHang
+            // 
+            this.txb_TongDonHang.Location = new System.Drawing.Point(680, 400);
+            this.txb_TongDonHang.Name = "txb_TongDonHang";
+            this.txb_TongDonHang.Size = new System.Drawing.Size(100, 22);
+            this.txb_TongDonHang.TabIndex = 4;
+            this.txb_TongDonHang.TextChanged += new System.EventHandler(this.txb_TongDonHang_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txb_TongDonHang);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.cb_XemTatCaDonHang);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox cb_XemTatCaDonHang;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txb_TongDonHang;
+    }
+}
+
---
+++ ThongTinDonHang/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThongTinDonHang
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void txb_TongDonHang_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void LoadInvoiceData(DateTime fromDate, DateTime toDate)
+        {
+            using (var context = new Model1())
+            {
+                // Lấy danh sách hóa đơn có ngày giao hàng nằm trong khoảng thời gian từ fromDate đến toDate
+                var invoices = context.Invoice
+                    .Where(i => i.DeliveryDate >= fromDate && i.DeliveryDate <= toDate)
+                    .Select(i => new
+                    {
+                        STT = i.InvoiceNo,
+                        NgayDatHang = i.OrderDate,
+                        NgayGiaoHang = i.DeliveryDate,
+                        ThanhTien = i.Order.Sum(o => o.Price * o.Quantity) // Tính tổng tiền mỗi hóa đơn
+                    })
+                    .ToList();
+
+                // Hiển thị dữ liệu trong DataGridView
+                dataGridView1.DataSource = invoices;
+
+                // Tính tổng cộng đơn hàng và hiển thị trong textbox txb_TongDonHang
+                decimal total = invoices.Sum(i => i.ThanhTien);
+                txb_TongDonHang.Text = total.ToString("C"); // Định dạng tiền tệ
+            }
+        }
+
+
+        private void cb_XemTatCaDonHang_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cb_XemTatCaDonHang.Checked)
+            {
+                // Lấy ngày đầu tháng và cuối tháng
+                DateTime firstDayOfMonth = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
+                DateTime lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddDays(-1);
+
+                // Cập nhật DateTimePicker và tải dữ liệu
+                dateTimePicker1.Value = firstDayOfMonth;
+                dateTimePicker2.Value = lastDayOfMonth;
+            }
+
+            // Tải lại dữ liệu
+            LoadInvoiceData(dateTimePicker1.Value, dateTimePicker2.Value);
+        }
+
+   
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dateTimePicker1.Value = DateTime.Today;
+            dateTimePicker2.Value = DateTime.Today;
+
+            // Gọi hàm để tải dữ liệu
+            LoadInvoiceData(dateTimePicker1.Value, dateTimePicker2.Value);
+        }
+
+       
+
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            LoadInvoiceData(dateTimePicker1.Value, dateTimePicker2.Value);
+        }
+       
+
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            LoadInvoiceData(dateTimePicker1.Value, dateTimePicker2.Value);
+        }
+    }
+}
